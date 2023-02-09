3eab886864295d7940547ecae6ea9595680514ee
iugo <iugogogo@gmail.com>
2023-02-09 11:24:19 +0800

    feat: add log

--- src/cli.ts
+++ src/cli.ts
@@ -53,15 +53,60 @@ if (!domain || !token) {
 }
 
 console.log({ domain, token });
+/**
+ * Number of successes
+ */
+let successTime = 0;
+/**
+ * Number of failures
+ */
+let failTime = 0;
+/**
+ * Last successful time
+ */
+let lastSuccessAt: Date | null = null;
+/**
+ * Time of last failure
+ */
+let lastFailAt: Date | null = null;
+/**
+ * Last successful information
+ */
+let lastSuccessInfo = '';
+/**
+ * Information about the last failure
+ */
+let lastFailInfo = '';
+
+const displayInfoLoopTime = 60000;
 
 setInterval(async () => {
   const [err, res] = await checkIP(token, domain, { timeout });
   const d = new Date();
   if (err) {
+    failTime += 1;
+    lastFailAt = d;
+    lastFailInfo = String(err);
     console.error(d, err);
   } else {
-    if (res !== 'eq') {
-      console.log(d, res);
-    }
+    successTime += 1;
+    lastSuccessAt = d;
+    lastSuccessInfo = res;
+  }
+  if (
+    delay > displayInfoLoopTime ||
+    Math.random() < delay / displayInfoLoopTime
+  ) {
+    console.log({
+      successTime,
+      failTime,
+      lastSuccessAt: lastSuccessAt?.toString(),
+      lastFailAt: lastFailAt?.toString(),
+      rate: new Intl.NumberFormat('zh-CN', { style: 'percent' }).format(
+        successTime / (successTime + failTime),
+      ),
+      lastSuccessInfo,
+      lastFailInfo,
+    });
   }
 }, delay);
